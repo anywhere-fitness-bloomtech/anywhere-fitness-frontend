cbaf0ab280ca7c8b38bd8e9e7d41d6804e201fe0
vladimir <vladimirsaakian@gmail.com>
2022-02-02 13:41:12 -0800

    registration and login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,72 @@
-const Login = () => {
-  return <div>Login</div>;
-};
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default Login;
+export default function Login() {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { push } = useNavigate();
+
+  const onChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+  function validateForm() {
+    return values.username.length > 0 && values.password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios
+      .post(
+        
+        values
+      )
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res.data.token);
+        push("/products");
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="register-form">
+        <h1 className="login">Login</h1>
+          <div className='form-group'>
+            <label className="username">Username </label>
+              <input
+                type="text"
+                id="user-input"
+                name="username"
+                value={values.username}
+                onChange={onChange}
+              />
+              <div>
+              <label className="password">Password </label>
+              <input
+                type="password"
+                id="pass-input"
+                name="password"
+                value={values.password}
+                onChange={onChange}
+              />
+              </div>
+          </div>
+          <div className="sub-button">
+        <button disabled={!validateForm()} className='login-button'>Login</button>
+        </div>
+      </form>
+
+    </div>
+  );
+}
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,122 @@
-const Register = () => {
-  return <div>Register</div>;
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+
+const initialValues = {
+  username: "",
+  password: "",
+  role: "",
 };
 
-export default Register;
+const initialErrors = {
+  username: "",
+  password: "",
+  role: "",
+};
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .trim()
+    .required("Username is required")
+    .min(3, "Username has to be at least three characters"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(6, "Password has to be at least six characters"),
+  role: yup
+    .string()
+    .required("Role is required!")
+    
+      
+});
+
+export default function Register() {
+  const [user, setUser] = useState(initialValues);
+  const [errors, setErrors] = useState({ initialErrors });
+  const [disabled, setDisabled] = useState(false);
+
+  const { push } = useNavigate();
+
+  const validation = (name, value) => {
+    yup
+      .reach(validationSchema, name)
+      .validate(value)
+      .then(() => setErrors({ ...initialErrors, [name]: "" }))
+      .catch((err) => setErrors({ ...initialErrors, [name]: err.errors[0] }));
+  };
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    validation(name, value);
+    setUser({ ...user, [name]: value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post(
+        
+        user
+      )
+      .then((res) => {
+        setUser(initialValues);
+        push("/login");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    validationSchema.isValid(user).then((valid) => setDisabled(!valid));
+  }, [user]);
+
+  return (
+    <div className="register-container">
+      <form onSubmit={onSubmit} className="register-form">
+        <div className="form-group">
+          <h1 className="register">Registration</h1>
+
+          <div className="form-group">
+            <label className="username">Username </label>
+            <input
+              type="text"
+              name="username"
+              value={user.name}
+              onChange={onChange}
+              id='user-input'
+            />
+          <div>
+            <label className="password">Password </label>
+            <input
+              type="password"
+              name="password"
+              value={user.password}
+              onChange={onChange}
+              id='pass-input'
+            />
+          </div>
+            <br />
+            <label className="role">
+          <span className='input-name'>Select</span>
+          <select onChange={onChange} value={initialValues.role}  name='role'>
+            <option value=''>---Role---</option>
+            <option value='client'>Client</option>
+            <option value='instructor'>Instructor</option>
+          </select>
+        </label>
+          </div>
+        </div>
+        <div className="errors">
+          <div>{errors.username}</div>
+          <div>{errors.password}</div>
+        </div>
+        <div className="sub-button">
+        <button  disabled={disabled} className="register-button">Register</button>
+        </div>
+      </form>
+    </div>
+  );
+}
